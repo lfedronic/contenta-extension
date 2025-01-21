3e2bc2382110cd4b1966c1b56e063ad5eb18ab45
lfedronic <lfedronic@ucdavis.edu>
2025-01-20 17:39:18 -0800

    functional link-based automatic & manual saving

--- background.js
+++ background.js
@@ -1,120 +1,152 @@
 // CONSTANTS
-const LOADTIME = 250; // hard-code load-time, will need to change this to a more dynamic solution
-const DURATION_PERCENTAGE = 0.75; // percentage of video watched to trigger popup
+const LOADTIME = 1500; // hard-code load-time, will need to change this to a more dynamic solution
+const DURATION_PERCENTAGE = 0.15; // percentage of video watched to trigger popup
 
 // URL FILTERS
-watchFilter = { // filter for web page to run content script
+watchTimeFilter = { // filter for web page to run content script
     urls: [
       "https://www.youtube.com/api/stats/watchtime*",
     ]
 };
 
+videoFilter = {
+    url: [
+        {urlMatches : "https://www.youtube.com/watch"}
+        ]
+}
+
 // MESSAGE ROUTING
 const processMessage = {
-    "openVideoSuggestionPopup": handleOpenVideoSuggestionPopup,
     "getVideoInfo": handleGetVideoInfo,
     "saveVideo": handleSaveVideo,
     "getSavedVideos": handleGetSavedVideos,
-    "videoDuration": handleVideoDuration,
-    "sendingVideoInfo": handleSendingVideoInfo
+    "getVideoDuration": handleGetVideoDuration,
+    "sendingVideoInfo": handleSendingVideoInfo,
+    "saveSessionWatchtimes": handleSaveSessionWatchtimes
 }
 
 // MESSAGE HANDLERS
 
-function handleOpenVideoSuggestionPopup(message, sender, sendResponse) {
-    console.log("OPEN POPUP CALLED!!");
-    console.log(message.content);
-    const videoInfo = message.content.videoInfo;
-    chrome.storage.session.set({"currentVideoName": videoInfo.name});
-    chrome.action.setPopup({popup: "addVideoSuggestion.html"});
-    chrome.action.openPopup();
-    chrome.action.setPopup({popup: "dashboard.html"});
-    sendResponse({"status": "received"});
-}
-
 function handleGetVideoInfo(message, sender, sendResponse) {
     console.log("GET DATA CALLED!!!");
-    chrome.storage.session.get(["currentVideoName"], (result) => {
+    chrome.storage.session.get(["currentVideoName", "currentVideoUrl"], (result) => {
         console.log("Current video name: ", result.currentVideoName);
-        sendResponse({"content": result.currentVideoName});
+        sendResponse({"content": {"name": result.currentVideoName, "url": result.currentVideoUrl}});
     });
 }
 
 function handleSaveVideo(message, sender, sendResponse) {
     console.log("SAVE VIDEO CALLED!!!");
-    const videoName = message.content;
+    console.log("Requested from: ", sender);
 
-    if (!videoName) {
-        console.log("No video name provided");
-        sendResponse({"status": "error"});
-        return;
-    }
-    
     chrome.storage.local.get(["savedVideos"], (result) => {
-        const savedVideos = result.savedVideos || [];
-        savedVideos.push(videoName);
-        console.log(savedVideos);
-        chrome.storage.local.set({"savedVideos": savedVideos}, () => {
-            console.log("Video saved!");
+        chrome.storage.session.get(["currentVideoUrl", "currentVideoName"], (currentResult) => {
+            if (!currentResult.currentVideoName || !currentResult.currentVideoUrl) {
+                console.log("No video to save");
+                sendResponse({"status": "incomplete"});
+                return;
+            }
+            const savedVideos = result.savedVideos || {};
+            if (savedVideos.hasOwnProperty(currentResult.currentVideoUrl)) {
+                console.log("Video already saved");
+                sendResponse({"status": "duplicate"});
+                return;
+            }
+            savedVideos[currentResult.currentVideoUrl] = currentResult.currentVideoName;
+            console.log(savedVideos);
+            chrome.storage.local.set({"savedVideos": savedVideos}, () => {
+                console.log("Video saved!");
+            });
+            sendResponse({"status": "received"});
         });
+        
     });
-    sendResponse({"status": "received"});
 }
+    
 
 function handleGetSavedVideos(message, sender, sendResponse) {
     console.log("GET SAVED VIDEOS");
     chrome.storage.local.get(["savedVideos"], (result) => {
         console.log(result.savedVideos);
-        sendResponse(result.savedVideos || []);
+        sendResponse(result.savedVideos || {});
     });
 }
 
-function handleVideoDuration(message, sender, sendResponse) {
+function handleGetVideoDuration(message, sender, sendResponse) {
     console.log("GET DURATION CALLED!!!");
-    console.log(message.content);
-    sendResponse(message.content);
+    const videoInfo = JSON.parse(document.querySelector("#microformat > player-microformat-renderer > script").innerHTML); // simple way to get the video info
+    const duration = parseInt(videoInfo.duration.match(/\d+/)[0], 10);
+    chrome.storage.session.set({"currentVideoDuration": duration});
+    if (!duration) {
+        console.log("No duration found");
+        sendResponse({"status": "incomplete", "duration": null});
+        return;
+    }
+    sendResponse({"status": "done", "duration": duration});
+
 }
 
 function handleSendingVideoInfo(message, sender, sendResponse) {
-    console.log(message.content.videoInfo.name);
-    chrome.storage.session.set({"currentVideoName": message.content.videoInfo.name});
-    chrome.storage.session.set({"currentVideoUrl": message.content.videoInfo.embedUrl});
     const newWatchedTime = message.content.newWatchedTime;
-    const url = message.content.videoInfo.embedUrl;
-    const durationInSecs = parseInt(message.content.videoInfo.duration.match(/\d+/)[0], 10);
+    
+    const durationInSecs = message.content.videoDuration
+    const name = message.content.videoName;
+    const url = message.content.videoUrl;
+    let sessionWatchtimes = message.content.sessionWatchtimes || {};
 
-    if (!watchTimes.hasOwnProperty(url)) {
-        watchTimes[url] = newWatchedTime;
+    if (!sessionWatchtimes.hasOwnProperty(url)) {
+        sessionWatchtimes[url] = newWatchedTime;
+        console.log("New session video added")
     }
+    
     else {
-        const prevWatchTime = watchTimes[url];
-        watchTimes[url] += newWatchedTime;
+        console.log("Updating current session video");
+        const prevWatchTime = sessionWatchtimes[url];
+        sessionWatchtimes[url] += newWatchedTime;
         if (prevWatchTime + newWatchedTime >= DURATION_PERCENTAGE * durationInSecs) {
-            console.log("Video watched for more than 75% of its duration");
-            sendResponse({"status": "received", "do": "openSuggestedVideoPopup"});
-            return true;
-            //chrome.runtime.sendMessage({action: "openVideoSuggestionPopup", content: message.content.videoInfo});
+            console.log(`Video watched for more than ${DURATION_PERCENTAGE * 100}% of its duration`);
+            chrome.action.setPopup({popup: "addVideoSuggestion.html"});
+            chrome.action.openPopup();
+            chrome.action.setPopup({popup: "mediaManager.html"});
+            
         }
-        
+        const percentWatched = (prevWatchTime + newWatchedTime) / durationInSecs;
+        console.log("Percent watched: ", (percentWatched * 100).toFixed(2) );   
     }
-    console.log(watchTimes);
-    sendResponse({"status": "received", "do": "nothing"});
+    console.log(sessionWatchtimes );
+    sendResponse({"sessionWatchtimes": sessionWatchtimes, "do": "nothing"});
 }
+
+function handleSaveSessionWatchtimes(message, sender, sendResponse) {
+    console.log("Saving session watchtimes");
+    chrome.storage.session.set({"sessionWatchtimes": message.content});
+    sendResponse({"status": "received"});
+}
+
 
 // HELPER FUNCTIONS
 
-function getVideoInfo(newWatchedTime) {
-    const alternateVideoInfo = document.querySelector("#movie_player > div.html5-video-container > video"); // doesn't work at the moment
-    const videoInfo = JSON.parse(document.querySelector("#microformat > player-microformat-renderer > script").innerHTML); // simple way to get the video info
-    if (!videoInfo) {
-        return {"status": "error"};
-    }
-    chrome.runtime.sendMessage({action: "sendingVideoInfo", content: {"videoInfo": videoInfo, "newWatchedTime": newWatchedTime}}, (response) => {
+function getUpdatedVideoInfo() { // used when user goes from one youtube video to the next 
+    const videoInfo = document.querySelector("head > title").textContent.replace(" - YouTube", "");
+    chrome.storage.session.set({"currentVideoName": videoInfo.name, "currentVideoUrl": videoInfo.url});
+    return videoInfo;
+}
+
+
+function processWatchTime(newWatchedTime, videoName, videoUrl, videoDuration, sessionWatchtimes) {
+    chrome.runtime.sendMessage({action: "sendingVideoInfo", content: {"newWatchedTime": newWatchedTime, "videoName": videoName, "videoUrl": videoUrl, "videoDuration": videoDuration, "sessionWatchtimes": sessionWatchtimes}}, (response) => {
         if (response.do === "openSuggestedVideoPopup") {
-            chrome.runtime.sendMessage({action: "openVideoSuggestionPopup", content: {"videoInfo": videoInfo}});
+            console.log("Opening suggested video popup");
         };
+        console.log("Returning watchtimes: ", response.sessionWatchtimes);
+        chrome.runtime.sendMessage({action: "saveSessionWatchtimes", content: response.sessionWatchtimes});
     });
-    return {"status": "done", "videoInfo": videoInfo};
+}
+
+function getDuration() {
+    const videoInfo = JSON.parse(document.querySelector("#microformat > player-microformat-renderer > script").innerHTML); // simple way to get the video info
+    const duration = parseInt(videoInfo.duration.match(/\d+/)[0], 10);
+    return duration;
 }
 
 function extractWatchTime(url, regex){
@@ -124,11 +156,14 @@ function extractWatchTime(url, regex){
     if (match) {
         const stValues = match[1].split('%2C'); // Split st values by commas
         const etValues = match[2].split('%2C'); // Split et values by commas
+        let newWatchedTime = 0;
 
-        const startTime = Number(stValues[0]);
-        const endTime = Number(etValues[etValues.length - 1]);
-        const newWatchedTime = endTime - startTime;
-
+        for (let i = 0; i < stValues.length; i++) {
+            stValues[i] = parseInt(stValues[i], 10);
+            etValues[i] = parseInt(etValues[i], 10);
+            newWatchedTime += etValues[i] - stValues[i];
+        }
+        
         console.log("st:", stValues);
         console.log("et:", etValues);
         return newWatchedTime;
@@ -139,7 +174,6 @@ function extractWatchTime(url, regex){
 
 }
 
-let watchTimes = {};
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => { 
     processMessage[message.action](message, sender, sendResponse);
@@ -147,75 +181,130 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 
+// PROCESS WATCHTIME REQUESTS
+
 chrome.webRequest.onCompleted.addListener((details) => {
     const regex = /(?:^|&)st=([^&]*)&et=([^&]*)/; // Regular expression to extract st and et values
-    console.log("Watchtime Request!!!");
     console.log(details);
     const url = details.url;
-    
-    if (url.includes("el=adunit")) {
-        console.log("Currently playing ad");
-        return;
-    } else { // Get video info
-        const newWatchedTime = extractWatchTime(url, regex);
-        chrome.scripting.executeScript({
-            target : {tabId : details.tabId},
-            func : getVideoInfo,
-            args: [newWatchedTime]
-        });
-    }
-}, watchFilter);
 
+    chrome.storage.session.get(["currentVideoName", "currentVideoUrl", "currentVideoDuration", "sessionWatchtimes"], (result) => {
+        
+        if (url.includes("el=adunit")) {
+            console.log("Currently playing ad");
+            return;
+        } else { // Get video info  
+            const newWatchedTime = extractWatchTime(url, regex);
+            chrome.scripting.executeScript({
+                target : {tabId : details.tabId},
+                func : processWatchTime,
+                args: [newWatchedTime, result.currentVideoName, result.currentVideoUrl, result.currentVideoDuration, result.sessionWatchtimes || {}]     
+            });
+        }
+    });
+    
+    
+}, watchTimeFilter);
+
+
+// HANDLE EXTENSION OPENING
 
 chrome.action.onClicked.addListener((tab) => {
     console.log("Action clicked!");
-    chrome.scripting.executeScript({
-        target : {tabId : tab.id},
-        func : getVideoInfo,
-        args: [0]
-    }, (response) => {
-        console.log(response.result);
-        chrome.action.setPopup({popup: "dashboard.html"});
+    chrome.storage.session.get(["currentVideoName", "currentVideoUrl"], (result) => {
+        chrome.action.setPopup({popup: "mediaManager.html"});
         chrome.action.openPopup();
-        chrome.action.setPopup({popup: ''});
+        chrome.action.setPopup({popup: ''});  
     });
-    
     
 });
 
 
 
 
+// HANDLE ACTIVE VIDEO TRACKING
+
+
+function getActiveTabVideoInfo(tabId, callback) {
+   
+    let videoInfo = {};
+
+    chrome.tabs.get(tabId, (tab) => {
+        if (tab.url && tab.title) {   
+            videoInfo["url"] = tab.url;
+            videoInfo["title"] = tab.title;
+            console.log("Tab info from getActiveTab", tab);
+        } else {
+            callback(null);
+        }
+    });
+
+    chrome.scripting.executeScript({
+        target: {tabId: tabId},
+        func: getDuration
+    }, (response) => {
+        if (response) {
+            videoInfo["duration"] = response[0].result;
+            callback(videoInfo);
+        }
+        else {
+            callback(null);
+        }
+        
+    });
+}
+
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    getActiveTabVideoInfo(activeInfo.tabId, (tabInfo) => {
+        if (tabInfo) {
+            if (tabInfo.url.startsWith("https://www.youtube.com/watch")) {
+                chrome.storage.session.set({"currentVideoUrl": tabInfo.url, "currentVideoName": tabInfo.title.replace(" - YouTube", ""), "currentVideoDuration": tabInfo.duration});
+            }
+        }
+        else {
+            console.log("Tab Info is not accessible in onActivated.");
+            console.log(tabInfo);
+        }
+    });
+});
+
+chrome.webNavigation.onCompleted.addListener((details) => { // event listener for when the page is loaded
+    setTimeout(() => {
+        getActiveTabVideoInfo(details.tabId, (tabInfo) => {
+            if (tabInfo) {
+                chrome.storage.session.set({"currentVideoUrl": tabInfo.url, "currentVideoName": tabInfo.title.replace(" - YouTube", ""), "currentVideoDuration": tabInfo.duration});
+            }
+            else {
+                console.log("Tab Info is not accessible in web nav.");
+                console.log(tabInfo);
+            }
+        });
+    }, LOADTIME);
+   
+}, videoFilter);
+
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => { // event listener for when the page is updated (e.g. clicking on a video)
+    setTimeout(() => {
+        getActiveTabVideoInfo(details.tabId, (tabInfo) => {
+            if (tabInfo) {
+                console.log("Duration info: ", tabInfo.duration);
+                chrome.storage.session.set({"currentVideoUrl": tabInfo.url, "currentVideoName": tabInfo.title.replace(" - YouTube", ""), "currentVideoDuration": tabInfo.duration});
+            }
+            else {
+                console.log("Tab Info is not accessible in onHistoryUpdated.");
+            }
+        });
+    }, LOADTIME);
+
+}, videoFilter);
+
+
 
 
 // OLD CODE
 
-// const filter = { // filter for web page to run content script
-//   url: [
-//     {
-//         urlMatches: "https://www.youtube.com/watch*",
-//     }
-//   ]
-// };
+// const alternateVideoInfo = document.querySelector("#movie_player > div.html5-video-container > video"); // doesn't work at the moment
 
-
-// chrome.webNavigation.onCompleted.addListener(function(details) { // event listener for when the page is loaded
-//     setTimeout(() => {
-//         console.log("onCompleted");
-//     }, LOADTIME); // hard-code load-time, will need to change this to a more dynamic solution
-//     chrome.scripting.executeScript({
-//         target : {tabId : details.tabId},
-//         func : getVideoInfo,
-//       })}, filter);
     
 
-// chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) { // event listener for when the page is updated (e.g. clicking on a video)
-//     setTimeout(() => {
-//         console.log("onHistoryStateUpdated");
-//     }, LOADTIME); // hard-code load-time, will need to change this to a more dynamic solution
-//     console.log(details);
-//     chrome.scripting.executeScript({
-//         target : {tabId : details.tabId},
-//         func : getVideoInfo,
-//         args: [10]
-//       });
--- videoSuggestionPopup.js
+++ videoSuggestionPopup.js
@@ -1,9 +1,11 @@
 let videoName = "";
+let videoUrl = "";
 
 chrome.runtime.sendMessage({action: "getVideoInfo"}, (response) => {
     console.log("Data received in popup:", response);
     const showVideo= document.querySelector("#videoName");
-    videoName = response.content;
+    videoName = response.content.name;
+    videoUrl = response.content.url;
     showVideo.textContent = videoName;
 });
 
@@ -12,9 +14,11 @@ let saveVideoButton = document.querySelector("#saveVideo");
 saveVideoButton.addEventListener("click", () => {
     saveVideoButton.textContent = "Saved!";
     saveVideoButton.style.backgroundColor = "green";
-    chrome.runtime.sendMessage({ action: "saveVideo", content: videoName }, (response) => {
-        console.log("Video saved:", response);
-        
+    chrome.runtime.sendMessage({ action: "saveVideo", content: {"url" : videoUrl} }, (response) => {
+        if (response.status === "duplicate") {
+            saveVideoButton.textContent = "Already saved!";
+            saveVideoButton.style.backgroundColor = "red";
+        }
     });
     
     
